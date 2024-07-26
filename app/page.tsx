df83f1d16242695ea94e4651d968ac781e402d02
Bui-Hien <buixuanhienmy@gmail.com>
2024-07-27 02:10:35 +0700

    f

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import FormUploadFile from "@/components/FormUploadFile";
 export default function Home() {
     return (
         <div className="flex flex-col justify-center items-center mt-40">
-            <FormUploadFile/>
+            {/*<FormUploadFile/>*/}
 
         </div>
     );
